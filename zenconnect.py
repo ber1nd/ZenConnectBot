aa3425f39a45b5cc3f60b8dfd9ab1c4e6f9ffaa9
ber1nd <fabiodummyemail@gmail.com>
2024-08-17 16:29:02 +0400

    pvp and check stability

--- zenconnect.py
+++ zenconnect.py
@@ -651,19 +651,19 @@ def create_battle_view(challenger_name, challenger_hp, challenger_energy, oppone
     o_energy_bar = create_bar(opponent_energy, 100)
 
     battle_view = f"""
-卐 ZEN WARRIOR ARENA 卐
-
-⚪ {challenger_name}
-✦ HP  [{c_hp_bar}] {challenger_hp}/100
-✦ Chi [{c_energy_bar}] {challenger_energy}/100
-
-        ⚔  VS  ⚔
-
-⚪ {opponent_name}
-✦ HP  [{o_hp_bar}] {opponent_hp}/100
-✦ Chi [{o_energy_bar}] {opponent_energy}/100
-
-≼ Choose Your Path, Warrior ≽
+┏━━━━━━━━━ ZEN WARRIOR ARENA ━━━━━━━━━┓
+┃                                      ┃
+┃  ⚪ {challenger_name:<20}             ┃
+┃  ✦ HP  [{c_hp_bar}] {challenger_hp:3d}/100    ┃
+┃  ✦ Chi [{c_energy_bar}] {challenger_energy:3d}/100    ┃
+┃                                      ┃
+┃            ⚔  VS  ⚔                 ┃
+┃                                      ┃
+┃  ⚪ {opponent_name:<20}             ┃
+┃  ✦ HP  [{o_hp_bar}] {opponent_hp:3d}/100    ┃
+┃  ✦ Chi [{o_energy_bar}] {opponent_energy:3d}/100    ┃
+┃                                      ┃
+┗━━━━━ Choose Your Path, Warrior ━━━━━━┛
 """
     return battle_view
 
